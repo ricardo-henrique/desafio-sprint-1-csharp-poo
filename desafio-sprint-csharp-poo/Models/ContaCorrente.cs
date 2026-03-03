b6d4434a500ece6aadee4920f35df76fc5f18953
Ricardo Henrique <ricardohenrique2013.1@gmail.com>
2026-03-03 08:19:10 -0300

    implement checking account

--- desafio-sprint-csharp-poo/Models/ContaCorrente.cs
+++ desafio-sprint-csharp-poo/Models/ContaCorrente.cs
@@ -1,7 +1,27 @@
 namespace BedrockBankCorp.Models
 {
-    class ContaCorrente
+    class ContaCorrente : ContaBancaria
     {
+        private const decimal WithdrawalFee = 2.5m;
 
+        public ContaCorrente(int accNum, string name, decimal initialBalance) : base(accNum, name, initialBalance, "Conta Corrente")
+        {
+        }
+
+        public override bool Withdraw(decimal amount)
+        {
+            decimal totalToDebit = amount + WithdrawalFee;
+
+
+            if (totalToDebit > Balance)
+            {
+                Console.WriteLine($"Transação Negada: Saldo insuficiente");
+                return false;
+            }
+
+            Balance -= totalToDebit;
+            Console.WriteLine($"Saque de R${amount:C} (taxa: {WithdrawalFee:C}). Seu saldo é: {Balance}");
+            return true;
+        }
     }
 }
